96dafdb259120399faf271985cfa3908cd68756a
sayrus383 <sayrus383@mail.ru>
2020-07-31 19:19:12 +0600

    Add ru

--- resources/lang/ru/fields.php
+++ resources/lang/ru/fields.php
@@ -1,7 +1,15 @@
 <?php
 
 return [
-    'SexID'   => 'Пол',
-    'Name'    => 'Имя',
-    'Surname' => 'Фамилия',
+    'SexID'       => 'Пол',
+    'Name'        => 'Имя',
+    'Surname'     => 'Фамилия',
+    'Model'       => 'Модель',
+    'IsRresident' => 'Резидент',
+    'IssueDate'   => 'Дата выпуска',
+    'Marka'       => 'Марка',
+    'Year'        => 'Год',
+    'Type'        => 'Тип',
+    'VIN'         => 'VIN',
+    'GRNZ'        => 'GRNZ',
 ];
